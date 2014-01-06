203b26de9d29c88c9950758048528f37dc58a41a
Rafal 'bl4de' Janicki <bloorq@gmail.com>
2014-01-07 00:32:14 +0100

    catalogs file

--- dirscanner.py
+++ dirscanner.py
@@ -6,18 +6,30 @@
 
 import httplib
 
-def scan(conn):
+def scan(conn, dirs):
+    iterator = 0
+    logfile = open("logfile.txt", "w+")
     for d in dirs:
         d = d.replace("\n", "")
         _url = "/cmsmadesimple/%s/" % (d)
         conn.request("GET", _url)
         response = conn.getresponse()
         response.read()
+        iterator += 1
+        if ( iterator % 1000 == 0):
+            print "%d scanned so far, scanning..." % (iterator)
+
         if (response.status == 200 or response.status == 302):
-            print "HTTP %s %s \t\t\turl is %s" % (response.status, response.reason, _url)
+            _output = "HTTP %s %s \t\t\turl is %s" % (response.status, response.reason, _url)
+            print _output
+            logfile.write(_output)
 
 
-dirs = open("folders.txt", "r")
-conn = httplib.HTTPConnection("localhost")
+def main():
+    dirs = open("catalogs.txt", "r")
+    conn = httplib.HTTPConnection("localhost")
 
-scan(conn)
+    scan(conn, dirs)
+    
+if __name__ == '__main__':
+    main()
