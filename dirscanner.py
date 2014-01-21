2c893c17a713c228d0d26d506748d7dc0adf69ca
Rafal 'bl4de' Janicki <bloorq@gmail.com>
2014-01-22 00:39:13 +0100

    passing hostname as argument; bugfixing

--- dirscanner.py
+++ dirscanner.py
@@ -6,24 +6,37 @@
 
 import httplib
 import catalogs
+import sys
+
 
 def scan(conn, dirs):
     iterator = 0
     logfile = open("logfile.txt", "a")
     for d in dirs:
         d = d.replace("\n", "")
-        _url = "/cmsmadesimple/%s/" % (d)
+        _url = "/%s" % (d)
+        _output = ""
+
+        print "checking %s" % (_url)
+
         conn.request("GET", _url)
         response = conn.getresponse()
         response.read()
         iterator += 1
-        if ( iterator % 1000 == 0):
+        if ( iterator % 100 == 0):
             print "%d scanned so far, scanning..." % (iterator)
 
         if (response.status == 200 or response.status == 302 or response.status == 304):
             _output = "HTTP %s %s \t\t\turl is %s" % (response.status, response.reason, _url)
+        if (response.status == 401):
+            _output = "HTTP %s %s \t\t\t, Unauthorized; url is %s" % (response.status, response.reason, _url)
+        if (response.status == 403):
+            _output = "HTTP %s %s \t\t\t, Needs authorization; url is %s" % (response.status, response.reason, _url)
+
+        if (_output != ""):
             print _output
             logfile.writelines(_output + "\n")
+
     logfile.close()
 
 
@@ -32,13 +45,13 @@ def main():
     dirs = open("catalogs.txt", "r")
     adminpanels = catalogs.adminpanels
 
-    conn = httplib.HTTPConnection("localhost")
+    conn = httplib.HTTPConnection(sys.argv[1])
 
     print "Trying to locate administration panel:"
     scan(conn, adminpanels)
 
-    print "Trying to find anything else..."
-    scan(conn, dirs)
+    # print "Trying to find anything else..."
+    # scan(conn, dirs)
 
     dirs.close()
     
